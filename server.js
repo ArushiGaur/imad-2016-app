0ba62d4daf23691eac681f8a25a21aae2ef41bd7
ArushiGaur <arushigaurhp@gmail.com>
2016-10-03 03:25:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/submit-name/:name', function (req , res){
-    var name = request.params.name;
+    var name = req.params.name;
     
     names.push(name);
     res.send(JSON.stringify(names));
