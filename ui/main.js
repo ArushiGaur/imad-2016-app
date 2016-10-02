bd89849469dbd9d49f5d666d971b3437ffdef758
ArushiGaur <arushigaurhp@gmail.com>
2016-10-03 03:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,15 +24,15 @@ button.onclick = function (){
      };
     
     
-    
+var nameInput = document.getElementById('name');
+var name = nameInput.value;    
     
   request.open('GET' , 'http://http://arushigaur.imad.hasura-app.io/submit-name?name=' + name , true);
   request.send(null);
  
     
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
    
